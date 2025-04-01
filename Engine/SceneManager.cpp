1cd4d7aadc8def13cd8c93dc9d6a8f4517457c21
ohtak6843_Laptop <ohtak6843@gmail.com>
2025-04-01 14:24:31 +0900

    마우스 움직임 추가
    좌클릭 시 마우스로 화면전환 가능

--- Engine/SceneManager.cpp
+++ Engine/SceneManager.cpp
@@ -331,7 +331,7 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 	//		gameObject->SetCheckFrustum(false);
 	//		//gameObject->GetTransform()->SetLocalPosition(Vec3(0.f, 0.f, 300.f));
 	//		//gameObject->GetTransform()->SetLocalScale(Vec3(1.f, 1.f, 1.f));
-	//		//gameObject->GetTransform()->SetLocalRotation(Vec3(-3.14 / 2, 0.f, 0.f));
+	//		//gameObject->GetTransform()->SetLocalRotation(Vec3(-90.f, 0.f, 0.f));
 	//		scene->AddGameObject(gameObject);
 	//		gameObject->AddComponent(make_shared<TestDragon>());
 	//	}
