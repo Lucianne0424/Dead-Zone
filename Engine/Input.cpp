610e377f8031858aab060b5bdfe53c0882ec4fbb
ohtak6843_Desktop <ohtak6843@gmail.com>
2025-05-09 19:23:19 +0900

    입력받을때 while문으로 받기

--- Engine/Input.cpp
+++ Engine/Input.cpp
@@ -106,15 +106,53 @@ void Input::Update()
 	HRESULT hr;
 	if (_mouse == nullptr || _keyboard == nullptr) return;
 
-	if (FAILED(hr = _mouse->GetDeviceState(sizeof(DIMOUSESTATE), &_DIMouseState)))
+	hr = _mouse->GetDeviceState(sizeof(DIMOUSESTATE), &_DIMouseState);
+	if (FAILED(hr))
 	{
-		while (_mouse->Acquire() == DIERR_INPUTLOST);
+		bool flag = false;
+		while (true)
+		{
+			hr = _mouse->Acquire();
+			if (SUCCEEDED(hr)) break; // 성공하면 탈출
+
+			// 다른 앱이 우선권을 가졌을 경우엔 재시도 불가
+			if (hr == DIERR_OTHERAPPHASPRIO)
+				break;
+
+			if (hr != DIERR_INPUTLOST)
+			{
+				flag = true;
+				break;
+			}
+		}
+
+		if (flag)
+			_mouse->GetDeviceState(sizeof(DIMOUSESTATE), &_DIMouseState);
 	}
 
 	BYTE asciiKeys[KEY_TYPE_COUNT] = {};
-	if (FAILED(hr = _keyboard->GetDeviceState(KEY_TYPE_COUNT, &asciiKeys)))
+	hr = _keyboard->GetDeviceState(KEY_TYPE_COUNT, &asciiKeys);
+	if (FAILED(hr))
 	{
-		while (_keyboard->Acquire() == DIERR_INPUTLOST);
+		bool flag = false;
+		while (true)
+		{
+			hr = _keyboard->Acquire();
+			if (SUCCEEDED(hr)) break; // 성공하면 탈출
+
+			// 다른 앱이 우선권을 가졌을 경우엔 재시도 불가
+			if (hr == DIERR_OTHERAPPHASPRIO)
+				break;
+			if (hr != DIERR_INPUTLOST)
+			{
+				flag = true;
+				break;
+			}
+		}
+
+		// 성공 후 다시 GetDeviceState 시도
+		if(flag)
+			_keyboard->GetDeviceState(KEY_TYPE_COUNT, &asciiKeys);
 	}
 
 	BYTE mouseButtons[3];
